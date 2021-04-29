b6b1228ec19f2fd93da10dd3963bc5e3f9e99b4c
IoanLT <ioan.tranole@gmail.com>
2021-04-29 08:59:46 +0100

    creating api.js file to store the time/urls and creating a .env file to store the API key

---
+++ src/api.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+import moment from "moment";
+
+// Store the date
+const today = moment().format("YYYY-MM-DD");
+const prevYear = moment().subtract(1, "year").format("YYYY-MM-DD");
+// const nextYear = moment().add(1, "year").format("YYYY-MM-DD");
+
+const apiKey = process.env.REACT_APP_RAWG_API;
+
+// Store our api endpoints
+const baseURL = "https://api.rawg.io/api/";
+const popularGames = `games?key=${apiKey}&dates=${prevYear},${today}&ordering=-rating&page_size=20`;
+
+export const popularGamesURL = `${baseURL}${popularGames}`;
+
+
